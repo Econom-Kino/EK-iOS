61600b3b962ad88cb02b6685716e4f3b19457db5
Yaroslav Kukhar <kukhar.yr@gmail.com>
2020-06-13 10:56:14 +0300

    updated movie detail header

--- Econom-Kino/Views/Components/MovieDetailHeaderView.swift
+++ Econom-Kino/Views/Components/MovieDetailHeaderView.swift
@@ -10,33 +10,34 @@ import SwiftUI
 import SDWebImageSwiftUI
 
 struct HeaderMovieDetailView: View {
-    @State var show = true
+    @Binding var movie: Movie
+    @Binding var show: Bool
+
     
     var body: some View {
         VStack () {
-            Text("Месники: Завершення")
-                .font(.system(size: self.show ? 32 : 21))
+            Text("\(movie.name)")
+                .font(.system(size: 32))
                 .fontWeight(.medium)
                 .foregroundColor(Color.white)
-                .lineLimit(self.show ? 10 : 1)
-                .frame(width: UIScreen.width - 30, alignment: .leading)
-                .padding(.leading, 30)
+                .lineLimit(self.show ? 3 : 1)
+                .scaleEffect(self.show ? 1 : 0.6)
             
             if self.show {
                 ZStack (alignment: .bottom) {
-                    WebImage(url: URL(string: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/w0TeZ0oGijYVubQYAakm7eo41Gn.jpg"))
+                    WebImage(url: URL(string: "\(movie.poster_link)"))
                         .resizable()
                         .aspectRatio(contentMode: .fit)
                         .frame(width: UIScreen.width / 2.3)
                         .cornerRadius(25)
-                        .shadow(color: Color.white.opacity(0.2), radius: 10, x: 2, y: 2)
+                        .shadow(color: Color.white.opacity(0.1), radius: 10, x: 2, y: 2)
                     Button(action: {
                         print("Trailer button pressed")
                     }) {
                         ZStack (alignment: .leading) {
                             Rectangle()
                                 .fill(Color.mainGray)
-                                .opacity(0.65)
+                                .opacity(0.75)
                                 .frame(width: UIScreen.width / 2.3, height: 45)
                                 .cornerRadius(25, corners: [.bottomRight, .bottomLeft])
                             HStack (spacing: 30) {
@@ -50,10 +51,9 @@ struct HeaderMovieDetailView: View {
                             
                             
                         }.padding(.top, 10)
-
-                        
                     }.buttonStyle(PlainButtonStyle())
                 }
+                .transition(AnyTransition.opacity.combined(with: .move(edge: .top)))
                 
             }
             
@@ -61,15 +61,16 @@ struct HeaderMovieDetailView: View {
             
         }.padding()
         .frame(width: UIScreen.width)
-        .background(HeaderBgView())
+            .background(HeaderBgView(poster_link: movie.poster_link))
     }
 }
 
 struct HeaderBgView: View {
+    var poster_link: String
     
     var body: some View {
         ZStack {
-            WebImage(url: URL(string:      "https://image.tmdb.org/t/p/w600_and_h900_bestv2/w0TeZ0oGijYVubQYAakm7eo41Gn.jpg"))
+            WebImage(url: URL(string: "\(poster_link)"))
                 .resizable()
                 .blur(radius: 12)
                 .frame(width: UIScreen.width + 50)
