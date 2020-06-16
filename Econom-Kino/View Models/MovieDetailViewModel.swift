ef03c30235b942c3eb3b9620782d231a57f1a5c8
Yaroslav Kukhar <kukhar.yr@gmail.com>
2020-06-16 16:21:36 +0300

    Added extension which subscript String with an Int

--- Econom-Kino/View Models/MovieDetailViewModel.swift
+++ Econom-Kino/View Models/MovieDetailViewModel.swift
@@ -9,13 +9,35 @@
 import Foundation
 
 class MovieDetailViewModel: ObservableObject {
-    func movieGenresToString(genres: [Genre]) -> String {
-        var genresArray: [String] = []
+    @Published var sessions: [Session] = []
+    
+    
+    func fetchSessions() {
+        let apiUrl = "https://ekinoback.herokuapp.com/cinemas"
+        sessions.removeAll()
         
-        for genere in genres {
-            genresArray.append(genere.name)
-        }
+        guard let url = URL(string: apiUrl) else { return }
         
-        return genresArray.joined(separator: ", ")
+        URLSession.shared.dataTask(with: url) { (data, response, err) in
+            if let err = err {
+                print(err.localizedDescription)
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse else { return }
+            if response.statusCode == 200 {
+                guard let data = data else { return }
+                DispatchQueue.main.async {
+                    do {
+                        self.sessions = try JSONDecoder().decode([Session].self, from: data)
+                    } catch let err {
+                        print("Error: \(err)")
+                    }
+                }
+            }
+            else {
+                print("HTTPURLResponse code: \(response.statusCode)")
+            }
+        }.resume()
     }
 }
